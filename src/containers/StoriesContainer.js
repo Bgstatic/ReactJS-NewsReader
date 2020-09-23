0e6d018c16d14a71c9930002535e419993592288
Bgstatic <bilgehangecici@gmail.com>
2020-09-23 18:20:20 +0300

    Styles added.

--- src/containers/StoriesContainer.js
+++ src/containers/StoriesContainer.js
@@ -1,13 +1,29 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useMemo } from "react";
 import { Story } from "../components/Story";
 import { getStoryId } from "../services/HackerNewsApi";
+import { useInfiniteScroll } from "../hooks/useInfiniteScroll";
+import {
+  GlobalStyle,
+  StoriesContainerWrapper,
+} from "../styles/StoriesContainerStyles";
 
 export const StoriesContainer = () => {
+  const { count } = useInfiniteScroll();
   const [storyIds, setStoryIds] = useState([]);
 
   useEffect(() => {
     getStoryId().then((data) => setStoryIds(data));
   }, []);
 
-  return storyIds.map((storyId) => <Story key={storyId} storyId={storyId} />);
+  return (
+    <>
+      <GlobalStyle />
+      <StoriesContainerWrapper data-test-id="stories-container">
+        <h1>Hacker News Stories</h1>
+        {storyIds.slice(0, count).map((storyId) => (
+          <Story key={storyId} storyId={storyId} />
+        ))}
+      </StoriesContainerWrapper>
+    </>
+  );
 };
