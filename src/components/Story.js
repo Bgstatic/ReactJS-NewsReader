c14bf938f680cb063613bbb34ca237d80ab086d2
Bgstatic <bilgehangecici@gmail.com>
2020-09-23 18:16:42 +0300

    Styled with styled-componentes.

--- src/components/Story.js
+++ src/components/Story.js
@@ -1,5 +1,12 @@
 import React, { useEffect, useState } from "react";
+import { mapTime } from "../mappers/mapTime";
 import { getStory } from "../services/HackerNewsApi";
+import {
+  StoryTitle,
+  StoryWrapper,
+  StoryMeta,
+  StoryMetaElement,
+} from "../styles/StoryStyles";
 
 export const Story = ({ storyId }) => {
   const [story, setStory] = useState({});
@@ -9,12 +16,21 @@ export const Story = ({ storyId }) => {
   }, []);
 
   return story && story.url ? (
-    <>
-      <a href={story.url}>
-        <p>{story.title}</p>
-      </a>
-      By: <p>{story.by}</p>
-      Posted: <p>{story.time}</p>
-    </>
+    <StoryWrapper data-testid="story">
+      <StoryTitle>
+        <a href={story.url} target="_blank">
+          {story.title}
+        </a>
+      </StoryTitle>
+      <StoryMeta>
+        <span data-testid="story-by">
+          <StoryMetaElement color="#000">By:</StoryMetaElement> {story.by}
+        </span>
+        <span data-testid="story-time">
+          <StoryMetaElement color="#000">Posted:</StoryMetaElement> {` `}
+          {mapTime(story.time)}
+        </span>
+      </StoryMeta>
+    </StoryWrapper>
   ) : null;
 };
