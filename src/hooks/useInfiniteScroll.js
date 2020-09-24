57b1fa9d0c00492cf8ec8b1e4727fe0ae0a2414f
Bgstatic <bilgehangecici@gmail.com>
2020-09-24 17:19:02 +0300

    Debounce.js helper function added.

--- src/hooks/useInfiniteScroll.js
+++ src/hooks/useInfiniteScroll.js
@@ -1,21 +1,21 @@
 import { useEffect, useState } from "react";
 import { STORY_INCREMENT, MAX_STORIES } from "../constants/index";
+import { debounce } from "../utils/debounce";
 
 export const useInfiniteScroll = () => {
   const [loading, setLoading] = useState(false);
   const [count, setCount] = useState(STORY_INCREMENT);
 
-  const handleScroll = () => {
+  const handleScroll = debounce(() => {
     if (
       window.innerHeight + document.documentElement.scrollTop !==
         document.documentElement.offsetHeight ||
       loading
     ) {
       return false;
-    } else {
-      setLoading(true);
     }
-  };
+    setLoading(true);
+  }, 500);
 
   useEffect(() => {
     if (!loading) {
